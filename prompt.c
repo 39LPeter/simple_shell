27d3c40d4be61710587c1f05a6c7e8274f55f88f
39LPeter <wlone3949@gmail.com>
2024-01-12 08:43:05 -0800

    shell project

--- prompt.c
+++ prompt.c
@@ -3,8 +3,10 @@
 void prompt(char **av,char **env)
 {
 	char *string *NULL;
-	int n=0;
+	int i ,status =0;
 	ssize_t num_char;
+	char *argv[] = {NULL,NULL};
+	pid_t child_pid;
 
   while(1)
 	
@@ -13,15 +15,29 @@ void prompt(char **av,char **env)
 
 	num_char *getline(&string, &n stdin);
 	if num_char ==-1;
-	(
+	{
 	 free(string)
 	 exit(EXIT_FAILURE);
-	)
+	 }
 	i=0;
 	while(string[i])
 	{
 		if (string[i] =='\n')
 		string[i] = 0;
 	}
-	} 
+	argv[0] = string;
+	child_pid = fork();
+	if (child_pid==-1)
+	{
+		free(string);
+	        exit(EXIT_FAILURE);
+	}
+	if(child_pid==0)
+	{
+		if(execve(argv[0],argv,env== -1))
+			printf("%s:NO SUCH DIRECTORY OR FILE\n" , av[0]);
+	}
+	else
+	        wait(&status);
+	}	
 }
--- shell.h
+++ shell.h
@@ -2,7 +2,8 @@
 #define SHELL_H
 
 #include <stdio.h>
-
+#include<stdlib.h>
+#include<unistd.h>
 void prompt(char **av, char **env)
 
 
